352dd383183d53c6bd3d56b6acbc4a9cb02ef007
Kamran Ali <kamranalinitb@gmail.com>
2020-01-05 17:38:46 +0530

    Added Typing in database module

--- database/book_sqlite.py
+++ database/book_sqlite.py
@@ -1,21 +1,25 @@
+from typing import List, Dict, Union
+
 from database_connection import DatabaseConnection
 
 """
 Concerned with storing and retrieving books from a database
 """
 
-def create_book_table():
+Book = Dict[str, Union[str, int]]
+
+def create_book_table() -> None: # " -> None" is called Type Hints
   with DatabaseConnection('resources/database/data.db') as connection:
     cursor = connection.cursor()
     cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
 
-def add_book(name, author):
+def add_book(name: str, author: str) -> None:
   with DatabaseConnection('resources/database/data.db') as connection:
     cursor = connection.cursor()
     cursor.execute('INSERT INTO books VALUES(?, ?, 0)', (name, author))
   
 
-def get_all_books():
+def get_all_books() -> List[Book]:
   with DatabaseConnection('resources/database/data.db') as connection:
     cursor = connection.cursor()
     
@@ -25,13 +29,13 @@ def get_all_books():
 
   return books
 
-def mark_book_as_read(name):
+def mark_book_as_read(name: str) -> None:
   with DatabaseConnection('resources/database/data.db') as connection:
     cursor = connection.cursor()
     cursor.execute('UPDATE books SET read=1 where name=?', (name,))
 
 
-def delete_book(name):
+def delete_book(name: str) -> None:
   with DatabaseConnection('resources/database/data.db') as connection:
     cursor = connection.cursor()
     cursor.execute('DELETE from books where name=?', (name,))
